195915fab65465b65f34f86635022514254d0ce5
Vlad Leica <cptvladski@gmail.com>
2022-03-06 16:50:38 +0200

    add mvp signal processing procedure

---
+++ analyse.py
@@ -0,0 +1,45 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+from scipy.linalg import dft
+from scipy.signal import find_peaks
+from scipy.fft import fft
+
+samplerate, data = wavfile.read('assets/sample.wav')
+
+print(len(data))
+print(samplerate)
+
+winsize = 1024
+wins = [data[x:x+winsize] for x in range(0, len(data), winsize)]
+
+notes = []
+for win in wins:
+    win = [x for x in win]
+    # plt.figure()
+    res = abs(fft(win))
+    res = res[0:(len(res) // 2)]
+    # plt.plot(res)
+    peaks = find_peaks(res, height=1.2 * 10 ** 5)
+    peak = None
+    peakval = 0
+    for i in range(len(peaks[0])):
+        idx = peaks[0][i]
+        val = peaks[1]['peak_heights'][i]
+        if val > peakval:
+            peak = idx
+            peakval = val
+    print(peak)
+    peak = peak if peak else 0
+    if peak < 1500:
+        print(f'Found freq peak at {peak * samplerate / winsize}')
+        notes += [peak * samplerate / winsize for _ in range(1024)]
+    else:
+        notes += [0 for _ in range(1024)]
+    # plt.show()
+
+plt.figure()
+plt.plot(data)
+plt.plot([note * 10 for note in notes])
+plt.show()
+# 39
