770ed8708a9b62315823a5276c40dc859c1ce4eb
Vlad Leica <cptvladski@gmail.com>
2022-03-06 17:21:49 +0200

    poll for notes from engine

--- src/main.c
+++ src/main.c
@@ -12,12 +12,13 @@
 #define CONSTANTS_IMPL
 #include "constants.h"
 #include "draw_util.h"
+#include "engine.h"
 #include "mem.h"
 #include "midi_read.h"
 #include "tui.h"
 
 // clock in ms
-uint64_t time_c = 0;
+uint64_t time_in_frames = 0;
 
 // time of day when program started in usec
 uint64_t start_usec = 0;
@@ -38,13 +39,11 @@ void display() {
     // Set every pixel in the frame buffer to the current clear color.
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // draw only the notes currently in flight
     for (int i = note_index; i < notes_no; i++) {
-        note_status status = draw_note(notes[i], time_c);
-        // printf(
-        //     "note index %d lane %d enter time_c %d at time_c %.3f status
-        //     %d\n", i, notes[i].lane, notes[i].enter_time, time_c * tpf,
-        //     status);
-        static_assert(STATES_NO == 3, "Handle all note statuses in display()");
+        note_status status = draw_note(notes[i], time_in_frames);
+        static_assert(STATUSES_NO == 3,
+                      "Handle all note statuses in display()");
         if (status == WAITING) {
             break;
         } else if (status == IN_FLIGHT) {
@@ -53,10 +52,12 @@ void display() {
             note_index++;
         }
     }
-
-    drawLegend(tines);
-    // drawAxes();
+    uint32_t notes_no = 0;
+    pitch *notes = engine_poll_notes(&notes_no);
+    draw_current_notes(notes, notes_no);
+    // draw supporting infrastructure
     drawTines();
+    drawLegend(tines);
     drawThreshold();
 
     // Flush drawing command buffer to make drawing happen as soon as possible.
@@ -64,12 +65,18 @@ void display() {
 }
 
 void timer(int v) {
+    // get current time of day and update time_in_frames with ms passed since
+    // the timer started
     struct timeval current;
     gettimeofday(&current, NULL);
     uint64_t current_usec = current.tv_sec * 1000000 + current.tv_usec;
-    time_c = ((current_usec - start_usec) / 1000000.0) * 60;
-    if (time_c % 60 == 0) printf("time_c is %ld\n", time_c);
+    time_in_frames = ((current_usec - start_usec) / 1000000.0) * 60;
+    if (time_in_frames % 60 == 0)
+        printf("time_in_frames is %ld\n", time_in_frames);
+
+    // trigger redisplay function
     glutPostRedisplay();
+    // set the timer for one frame in the future (@60fps)
     glutTimerFunc(1000.0 / 60.0, timer, v);
 }
 
@@ -87,6 +94,12 @@ void reshape(GLint w, GLint h) {
 // enters the main event loop.
 int main(int argc, char **argv) {
     init_mem();
+    engine_status status = engine_init();
+    if (status != ENGINE_OK) {
+        fprintf(stderr, "Engine failed to start with status %d", status);
+        end_mem();
+        exit(1);
+    }
     uint32_t track_number = 0;
     char *midi_file_name = tui(&track_number);
     if (!midi_file_name) {
@@ -95,7 +108,6 @@ int main(int argc, char **argv) {
     }
     printf("Selected %s track %p\n", midi_file_name, (void *)&track_number);
     tines = setup_tines();
-    // TODO unhardcode midi file
     notes = read_midi_file(midi_file_name, track_number, &notes_no, &bpm);
 
     // Use a single buffered window in RGB mode (as opposed to a double-buffered
@@ -132,6 +144,8 @@ int main(int argc, char **argv) {
     // Tell GLUT that whenever the main window needs to be repainted that it
     // should call the function display().
     glutDisplayFunc(display);
+    // Use the reshape function to preserve aspect ratio whenever window is
+    // reshaped
     glutReshapeFunc(reshape);
 
     // Setup perspective and lookAt so aspect ratio is preserved when resizing
@@ -149,7 +163,7 @@ int main(int argc, char **argv) {
     glDepthFunc(GL_LEQUAL);
 
     // setup ticking time
-    time_c = 0;
+    time_in_frames = 0;
     glutTimerFunc(1000.0 / 60.0, timer, 0);
     printf("First note on lane: %d\n", notes[0].lane);
     printf("C0 lane is %d", C1);
@@ -159,8 +173,18 @@ int main(int argc, char **argv) {
     gettimeofday(&start, NULL);
     start_usec = start.tv_sec * 1000000 + start.tv_usec;
 
+    // before starting the main loop, start the engine client's process callback
+    // function
+    if (engine_start() != ENGINE_STARTED) {
+        fprintf(stderr, "Failed to start engine\n");
+        end_mem();
+        exit(1);
+    }
+
     // Start the main loop
     glutMainLoop();
     printf("Main loop ended\n");
+    // clear memory before exiting
     end_mem();
+    return 0;
 }
