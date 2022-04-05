aa3afb849674027593f059d6de48567cdc5e4768
Vlad Leica <cptvladski@gmail.com>
2022-04-05 12:21:56 +0300

    generic use of dft

--- src/proc.c
+++ src/proc.c
@@ -1,15 +1,22 @@
 #include <complex.h>
 #include <jack/types.h>
 #include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "proc.h"
 // TODO: implement fft
-void dft(jack_nframes_t nframes, jack_default_audio_sample_t *indata,
-         jack_default_audio_sample_t *outdata) {
+void dft(uint32_t nframes, float *indata, float *outdata) {
+  // printf("dft()\n");
   for (uint32_t k = 0; k < nframes; k++) {
     double complex sumcomplex = 0;
     for (uint32_t t = 0; t < nframes; t++) {
       double fi = 2 * M_PI * t * k / nframes;
-      sumcomplex += indata[t] * cexp(-fi * I);
+      float temp = indata[t];
+      // sumcomplex += temp * cexp(-fi * I);
+      temp = temp * cexp(-fi * I);
+      sumcomplex += temp;
     }
     outdata[k] = cabs(sumcomplex);
   }
