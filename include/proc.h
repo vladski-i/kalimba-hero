577f0b3507740ca018a3f3b5e095756e3e1ff08c
Vlad Leica <cptvladski@gmail.com>
2022-04-05 12:21:26 +0300

    undo bad commit

--- include/proc.h
+++ include/proc.h
@@ -5,9 +5,4 @@
 
 void dft(uint32_t nframes, float *indata, float *outdata);
 
-typedef enum fft_dir {
-  FFT_FORWARD, /* kernel uses "-1" sign */
-  FFT_INVERSE  /* kernel uses "+1" sign */
-} fft_dir;
-
 #endif
