05a7b549cfdc84ec6002ebcf751efac842c64bc2
Vlad Leica <cptvladski@gmail.com>
2022-04-17 15:31:38 +0300

    added function to track memory with a custom desctructor

--- src/mem.c
+++ src/mem.c
@@ -3,15 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void **ptrs;
+typedef struct res_s{
+    void *ptr;
+    void (*free_fn)(void *);
+}res;
+
+static res **ptrs;
 static uint32_t current_size = 10;
 static uint32_t current_index;
 
-void init_mem() { ptrs = calloc(current_size, sizeof(void *)); }
+
+
+void init_mem() { ptrs = calloc(current_size, sizeof(res)); }
 
 void end_mem() {
     for (uint32_t i = 0; i < current_index; i++) {
-        free(ptrs[i]);
+        ptrs[i]->free_fn(ptrs[i]->ptr);
     }
     free(ptrs);
 }
@@ -20,15 +27,15 @@ static void check_realloc() {
     if (current_index >= current_size) {
         printf("[mem] reallocation, size from %d to %d\n", current_size,
                current_size * 2);
-        void **backup = ptrs;
-        ptrs = realloc(ptrs, current_size * 2 * sizeof(void *));
+        res **backup = ptrs;
+        ptrs = realloc(ptrs, current_size * 2 * sizeof(res));
         if (!ptrs) {
             ptrs = backup;
             end_mem();
             printf("Reallocation of memory failed\n");
             exit(1);
         }
-        memset(ptrs + current_index, 0, current_index * sizeof(void *));
+        memset(ptrs + current_index, 0, current_index * sizeof(res));
         current_size = current_size * 2;
     }
 }
@@ -36,13 +43,28 @@ static void check_realloc() {
 void *alloc(uint32_t nmemb, uint32_t size) {
     void *ptr = calloc(nmemb, size);
     check_realloc();
-    ptrs[current_index++] = ptr;
+    res *r = calloc(1,sizeof(res));
+    r->ptr = ptr;
+    r->free_fn = free;
+    ptrs[current_index++] = r;
+    return ptr;
+}
+
+void *alloc_res(void *ptr, void (*free_fn)(void *)){
+    check_realloc();
+    res *r = calloc(1,sizeof(res));
+    r->ptr = ptr;
+    r->free_fn = free_fn;
+    ptrs[current_index++] = r;
     return ptr;
 }
 
 char *strdup_m(char *src) {
     char *ptr = strdup(src);
     check_realloc();
-    ptrs[current_index++] = (void *)ptr;
+    res *r = calloc(1,sizeof(res));
+    r->ptr = ptr;
+    r->free_fn = free;
+    ptrs[current_index++] = r;
     return ptr;
 }
